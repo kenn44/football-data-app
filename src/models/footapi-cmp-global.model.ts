66ba67ad7ea61116fb096143112a0e94bfe763a2
kenn <kennethassogba@gmail.com>
2017-07-07 18:19:58 +0100

    Add an alternative model

--- src/models/footapi-cmp-global.model.ts
+++ src/models/footapi-cmp-global.model.ts
@@ -24,3 +24,48 @@ export class FootApiCmpGlobal {
   lastUpdated: Date;
 }
 
+
+
+/*
+declare module FootApiCmpGlobal {
+
+  export interface Self {
+    href: string;
+  }
+
+  export interface Teams {
+    href: string;
+  }
+
+  export interface Fixtures {
+    href: string;
+  }
+
+  export interface LeagueTable {
+    href: string;
+  }
+
+  export interface Links {
+    self: Self;
+    teams: Teams;
+    fixtures: Fixtures;
+    leagueTable: LeagueTable;
+  }
+
+  export interface RootObject {
+    _links: Links;
+    id: number;
+    caption: string;
+    league: string;
+    year: string;
+    currentMatchday: number;
+    numberOfMatchdays: number;
+    numberOfTeams: number;
+    numberOfGames: number;
+    lastUpdated: Date;
+  }
+
+}
+
+*/
+
