00f99ea95675b9987aa21e14cd63693ac0098724
kenn <kennethassogba@gmail.com>
2017-07-09 03:56:49 +0100

    Set the X-Auth-Token Headers :tada::tada::tada:

--- src/providers/foot-api-service/foot-api-service.ts
+++ src/providers/foot-api-service/foot-api-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
@@ -17,14 +17,20 @@ export class FootApiServiceProvider {
 
   private baseUrl: string = 'http://api.football-data.org/v1/';
 
+  private headers = new Headers({'X-Auth-Token': '60a0e2b63a304790b8354014846ebf7d'});
+
   constructor(public http: Http) {
     console.log('Hello FootApiServiceProvider Provider');
   }
 
   public getCompetions(): Promise<FootApiCmpGlobal>{
-    const url = `$(this.baseUrl)competitions`;
 
-    return this.http.get(url)
+    const url = `${this.baseUrl}competitions`;
+
+    /*let headers = new Headers();
+    headers.append('X-Auth-Token', '60a0e2b63a304790b8354014846ebf7d');*/
+
+    return this.http.get(url, {headers: this.headers})
       .toPromise()
       .then(response => response.json() as FootApiCmpGlobal)
       .catch(error => console.log("Error: " + error))
